4070289341231ed9d51a40b969eb520c263b922b
antonio-leblanc <antonioleblanc@poli.ufrj.br>
2021-05-13 12:47:33 -0300

    sass + loaders

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,14 @@ module.exports = {
           },
         ]
       },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ]
+      },
     ]
   },
   plugins: [
